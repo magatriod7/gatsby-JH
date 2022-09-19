68cb58a4f788e0ff525b532829631a1cc11656fa
JangHoon <magatriod7@naver.com>
2022-09-19 15:05:11 +0900

    Test: component

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import { container, container2 } from "./layout.module.css";
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
